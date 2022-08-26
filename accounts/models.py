86f4126af9fe37c394f3217e82a1158a8e5de071
Ivie Osoiye <ivieosoiye@gmail.com>
2022-08-26 14:28:14 +0100

    created a user account class

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User as auth_user, PermissionsMixin
 
 # Create your models here.
+class User(auth_user, PermissionsMixin):
+    
+    def __str__(self):
+        return f"@{self.username}"
