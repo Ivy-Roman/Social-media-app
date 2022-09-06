de35b48a0e5821b3c7ac9c0fd4c4a3b11fadd1d4
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-06 18:55:55 +0100

    imported reverse module

--- groups/models.py
+++ groups/models.py
@@ -1,5 +1,5 @@
-from email.policy import default
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 import misaka
 from django.contrib.auth import get_user_model
