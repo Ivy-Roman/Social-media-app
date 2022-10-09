fdfe1f1c6e2b41232e120af3f47f0b970dfb89b5
Ivie Osoiye <ivieosoiye@gmail.com>
2022-10-09 20:23:12 +0100

    changed path to re_path, as re_path is more compatible with regex

--- groups/urls.py
+++ groups/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import re_path
 from . import views
 
 app_name = 'groups'
 
 urlpatterns = [
-    path('^$', views.ListGroups.as_view(), name='all'),
-    path('^new/$', views.CreateGroup.as_view(), name='create'),
-    path('posts/in/(?P<slug>[-\w]+)/$', views.SingleGroup.as_view(), name='single')
+    re_path(r'^$', views.ListGroups.as_view(), name='all'),
+    re_path(r'^new/$', views.CreateGroup.as_view(), name='create'),
+    re_path(r'posts/in/(?P<slug>[-\w]+)/$', views.SingleGroup.as_view(), name='single')
 ]
 
