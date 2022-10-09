e4abd4b0932f2f4a668c9065d03c09d4d7026669
Ivie Osoiye <ivieosoiye@gmail.com>
2022-10-09 23:20:43 +0100

    fixed syntax error

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     re_path(r'new/$', views.CreatePost.as_view(), name='create'),
     re_path(r'by/(?P<username>[-\w]+)', views.UserPosts.as_view(), name='for_user'),
     re_path(r'by/(?P<username>[-\w]+)/(?P<pk>\d+/$)', views.PostDetail.as_view(), name='single'),
-    re_path('rdelete/(?P<pk>\d+/$)', views.DeletePost.as_view(), name='delete')
+    re_path(r'delete/(?P<pk>\d+/$)', views.DeletePost.as_view(), name='delete')
 ]
